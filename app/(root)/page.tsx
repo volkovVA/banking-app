e708aac420330a37f4092c7382b8082e68af9a87
Volkov Slava <slava@Slavas-MacBook-Pro.local>
2024-10-19 23:56:45 +0300

    feat: add home page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
-import React from "react"
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
 
 const Home = () => {
+	const loggedIn= { firstName: 'Slava' }
+
 	return (
-		<div>
-			Home
-		</div>
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Welcome"
+						subtext="Access and manage your account and transactions efficiently"
+						user={loggedIn?.firstName || 'Guest'}
+					/>
+
+					<TotalBalanceBox
+						accounts={[]}
+						totalBanks={1}
+						totalCurrentBalance={2500}
+					/>
+				</header>
+			</div>
+		</section>
 	)
 }
 
