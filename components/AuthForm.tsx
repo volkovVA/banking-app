df4143e381fac31c3a627df7853400cfc646c476
Volkov Slava <slava@Slavas-MacBook-Pro.local>
2024-11-04 21:56:27 +0300

    feat: add auth page code

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,8 +12,10 @@ import { Form } from "@/components/ui/form";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const AuthForm = ({ type }: { type: string }) => {
+	const router = useRouter();
 	const [user, setUser] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
 
@@ -27,10 +29,32 @@ const AuthForm = ({ type }: { type: string }) => {
     },
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async(data: z.infer<typeof formSchema>) => {
 		setIsLoading(true);
-    console.log(values);
-		setIsLoading(false);
+
+		try {
+			// Sign up with Appwrite & create plain link token
+
+			if (type === 'sign-up') {
+				// const newUser = await signUp(data);
+
+				// setUser(newUser);
+			}
+
+			if (type === 'sign-in') {
+				// const response = await SignIn({
+				// 	email: data.email,
+				// 	password: data.password
+				// })
+
+				// if (response) router.push('/');
+			}
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setIsLoading(false);
+		}
+
   }
 	
 	return (
@@ -96,6 +120,12 @@ const AuthForm = ({ type }: { type: string }) => {
 										name="address1"
 										label="Address"
 										placeholder="Enter your specific address"
+									/>
+									<CustomInput
+										control={form.control}
+										name="city"
+										label="City"
+										placeholder="Enter your city"
 									/>
 									<div className="flex gap-4">
 										<CustomInput
